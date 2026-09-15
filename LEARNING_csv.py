fbe3599d9b7b95f6c2c3d4a79105a7a835fdf8bb
caf1208 <charlesafrankland@gmail.com>
2026-09-15 12:52:55 +0100

    Update LEARNING_csv.py

--- LEARNING_csv.py
+++ LEARNING_csv.py
@@ -3,7 +3,8 @@ file = open("csvTeachMyself/teams.csv","r",newline="")
 reader = csv.reader(file)
 next(reader)
 for row in reader:
+    team = row[0]
     attack = float(row[1])
     defence = float(row[2])
-    print(row[0],attack,defence)
-## Now attack and defence strengths are numbers instead of strings
+    print(f"{team}: \n attack ={attack} \n defence ={defence}")
+## my way of learning new line at the same time as doing what i did before
