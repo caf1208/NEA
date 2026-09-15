b8856bccabb263963e43b26c3cf84b7495486f83
caf1208 <charlesafrankland@gmail.com>
2026-09-15 14:56:49 +0100

    Update ACTUAL_csv.py

--- ACTUAL_csv.py
+++ ACTUAL_csv.py
@@ -5,7 +5,10 @@ next(reader)
 gScoredArsenal=0
 countArsenal=0
 gConcArsenal=0
+homeGoals=0
+gamesTotal=0
 for row in reader:
+    """
     homeGoals = float(row[5])
     awayGoals = float(row[6])
     if row[3] == "Arsenal":
@@ -18,4 +21,8 @@ for row in reader:
         gConcArsenal+=homeGoals
 print("goals for per game:", round(gScoredArsenal/countArsenal,2))
 print("goals against per game:", round(gConcArsenal/countArsenal,2))
-### attack strength and def strength
+"""
+    gamesTotal+=1
+    homeGoals+=float(row[5])
+print("home goals per game:", round(homeGoals/gamesTotal,2))
+##average home goals, different part of poisson formula, will start sqlite soon
