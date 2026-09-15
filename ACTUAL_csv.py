63d858c9d8d47d739b59331a14ebd76c97e45d44
caf1208 <charlesafrankland@gmail.com>
2026-09-15 14:22:07 +0100

    Calculate Arsenal's total goals from CSV data
    Count Arsenal's goals from home and away games in the CSV.

--- ACTUAL_csv.py
+++ ACTUAL_csv.py
@@ -2,10 +2,14 @@ import csv
 file = open("2526.csv","r",newline="")
 reader = csv.reader(file)
 next(reader)
-count = 0
+goalsArsenal=0
 for row in reader:
-    if row[3] == "Arsenal" or row[4] == "Arsenal":
-        count +=1
-print(f"Arsenal played {count} games")
-
-### i plan to document it but it is here too just incase it needs to be
+    homeGoals = float(row[5])
+    awayGoals = float(row[6])
+    if row[3] == "Arsenal":
+        goalsArsenal+=homeGoals
+    elif row[4] == "Arsenal":
+        goalsArsenal+=awayGoals
+print(goalsArsenal)
+### shows that I can make use of the values in the csv, obviously not fully developed yet but for documentation purposes
+##reason the file is different to the github one is beacuse i am coding through idle rather than codespace since i cant figure visualstudio out
