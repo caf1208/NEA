c2fbca754eae52a67a29d97906ad0134d12501a2
caf1208 <charlesafrankland@gmail.com>
2026-09-15 14:08:26 +0100

    learning how to use this individual csv
    Add functionality to count games played by Arsenal from CSV.

--- ACTUAL_csv.py
+++ ACTUAL_csv.py
@@ -1,1 +1,11 @@
+import csv
+file = open("2526.csv","r",newline="")
+reader = csv.reader(file)
+next(reader)
+count = 0
+for row in reader:
+    if row[3] == "Arsenal" or row[4] == "Arsenal":
+        count +=1
+print(f"Arsenal played {count} games")
 
+### i plan to document it but it is here too just incase it needs to be
