6c455b27ca3fb0d484e16be539403ba36f6fbe95
caf1208 <charlesafrankland@gmail.com>
2026-09-15 16:34:22 +0100

    bigger project

--- ACTUAL_csv.py
+++ ACTUAL_csv.py
@@ -2,13 +2,14 @@ import csv
 file = open("2526.csv","r",newline="")
 reader = csv.reader(file)
 next(reader)
+"""
 gScoredArsenal=0
 countArsenal=0
 gConcArsenal=0
 homeGoals=0
 gamesTotal=0
 for row in reader:
-    """
+    """"""
     homeGoals = float(row[5])
     awayGoals = float(row[6])
     if row[3] == "Arsenal":
@@ -21,8 +22,64 @@ for row in reader:
         gConcArsenal+=homeGoals
 print("goals for per game:", round(gScoredArsenal/countArsenal,2))
 print("goals against per game:", round(gConcArsenal/countArsenal,2))
-"""
+""""""
     gamesTotal+=1
     homeGoals+=float(row[5])
 print("home goals per game:", round(homeGoals/gamesTotal,2))
-##average home goals, different part of poisson formula, will start sqlite soon
+"""
+def chooseTeam(teams):
+    team=input("Which team")
+    if team.lower() in teams:
+        return team.lower()
+    else:
+        return chooseTeam(teams)
+def calculateTeamStrengths(team,reader):
+    teamHGames=0
+    teamHGoalsF=0
+    teamHGoalsA=0
+    teamAGames=0
+    teamAGoalsF=0
+    teamAGoalsA=0
+    games=0
+    AGoals=0
+    HGoals=0
+    for row in reader:
+        hg=int(row[5])
+        ag=int(row[6])
+        games+=1
+        AGoals+=ag
+        HGoals+=hg
+        if team==row[3].lower():
+            teamHGames+=1
+            teamHGoalsF+=hg
+            teamHGoalsA+=ag
+        elif team==row[4].lower():
+            teamAGames+=1
+            teamAGoalsF+=ag
+            teamAGoalsA+=hg
+    avgH=HGoals/games
+    avgA=AGoals/games
+    print(f"""
+league away avg goals: {round(avgA,2)}
+league home avg goals: {round(avgH,2)}
+
+{team.capitalize()}:
+
+home attack strength: {round((teamHGoalsF/teamHGames)/avgH,2)}
+home defense strength: {round((teamHGoalsA/teamHGames)/avgA,2)}
+away attack strength: {round((teamAGoalsF/teamAGames)/avgA,2)}
+away defence strength: {round((teamAGoalsA/teamAGames)/avgH,2)}
+""")
+teams=[]
+for row in reader:
+    if row[3].lower() not in teams:
+        teams.append(row[3].lower())
+    if row[4].lower() not in teams:
+        teams.append(row[4].lower())
+file.seek(0)
+reader=csv.reader(file)
+next(reader)
+calculateTeamStrengths(chooseTeam(teams),reader)
+### this i did for fun and a basis for OOP when i begin to do that
+
+        
