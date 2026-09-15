21c1b66c1a77f85e10d9891b92e97e88043a2e7f
caf1208 <charlesafrankland@gmail.com>
2026-09-15 14:33:45 +0100

    Update ACTUAL_csv.py

--- ACTUAL_csv.py
+++ ACTUAL_csv.py
@@ -3,13 +3,15 @@ file = open("2526.csv","r",newline="")
 reader = csv.reader(file)
 next(reader)
 goalsArsenal=0
+countArsenal=0
 for row in reader:
     homeGoals = float(row[5])
     awayGoals = float(row[6])
     if row[3] == "Arsenal":
         goalsArsenal+=homeGoals
+        countArsenal+=1
     elif row[4] == "Arsenal":
         goalsArsenal+=awayGoals
-print(goalsArsenal)
-### shows that I can make use of the values in the csv, obviously not fully developed yet but for documentation purposes
-##reason the file is different to the github one is beacuse i am coding through idle rather than codespace since i cant figure visualstudio out
+        countArsenal+=1
+print("goals per game:", round(goalsArsenal/countArsenal,2))
+#### calculates arsenals attack strength which is part of the poisson formula
