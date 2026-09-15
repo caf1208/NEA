25c99cde5d6add407f04371b349f1bff5ebfd212
caf1208 <charlesafrankland@gmail.com>
2026-09-15 12:22:08 +0100

    Modify CSV output to skip headers
    Added condition to skip printing headers in CSV output.

--- LEARNING_csv.py
+++ LEARNING_csv.py
@@ -2,4 +2,5 @@ import csv
 file = open("csvTeachMyself/teams.csv","r",newline="")
 reader = csv.reader(file)
 for row in reader:
-  print(row)
+    if row !=1:
+        print(row[0],"has an attack strength of", row[1])   #  trying to print without it printing headers
