8ab30814f4442a7d0f65fa1df85d4d65539265ed
caf1208 <charlesafrankland@gmail.com>
2026-09-15 12:23:38 +0100

    Print all rows from CSV including headers
    Removed condition to print all rows including headers.

--- LEARNING_csv.py
+++ LEARNING_csv.py
@@ -1,6 +1,6 @@
 import csv
 file = open("csvTeachMyself/teams.csv","r",newline="")
 reader = csv.reader(file)
+next(reader)     #    skips the first line ie the header
 for row in reader:
-    if row !=1:
-        print(row[0],"has an attack strength of", row[1])   #  trying to print without it printing headers
+    print(row[0],"has an attack strength of", row[1])
