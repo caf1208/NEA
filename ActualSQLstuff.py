a90387537ff7138ca790af9099c93c06edcbad9a
caf1208 <charlesafrankland@gmail.com>
2026-09-18 09:04:00 +0100

    find averages for anything ->> team strengths next

--- ActualSQLstuff.py
+++ ActualSQLstuff.py
@@ -15,12 +15,35 @@ CREATE TABLE IF NOT EXISTS matches(
 )
 """)
 con.commit()
-for row in reader:
-    goalsHome=int(row[5])
-    goalsAway=int(row[6])
-    cur.execute("""
-INSERT INTO matches VALUES (?,?,?,?)
-""",(row[3],row[4],goalsHome,goalsAway))
-con.commit()
+cur.execute("""
+SELECT COUNT(*) FROM matches WHERE homeTeam="Arsenal"
+""")
+homeGames=cur.fetchone()[0]
+cur.execute("""
+SELECT COUNT(*) FROM matches WHERE awayTEAM="Arsenal"
+""")
+awayGames=cur.fetchone()[0]
+cur.execute("""
+SELECT SUM(homeGoals) FROM matches WHERE homeTeam="Arsenal"
+""")
+homeGoalsF=cur.fetchone()[0]
+cur.execute("""
+SELECT SUM(awayGoals) FROM matches WHERE homeTeam="Arsenal"
+""")
+homeGoalsA=cur.fetchone()[0]
+cur.execute("""
+SELECT SUM(awayGoals) FROM matches WHERE awayTeam="Arsenal"
+""")
+awayGoalsF=cur.fetchone()[0]
+cur.execute("""
+SELECT SUM(homeGoals) FROM matches WHERE awayTeam="Arsenal"
+""")
+awayGoalsA=cur.fetchone()[0]
+print(f"""
+Home Goals Scored avg: {round(homeGoalsF/homeGames,2)}
+Home Goals Conceded avg: {round(homeGoalsA/homeGames,2)}
+Away Goals Scored avg: {round(awayGoalsF/awayGames,2)}
+Away Goals Conceded avg: {round(awayGoalsA/awayGames,2)}
+""")
 con.close()
-
+#in process of doing same using sql as with csv
